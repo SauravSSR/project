e5b2e1814985ad9ac7b8b5a85cf11eeffaef24bc
SauravSSR <sauravsinghraina@gmail.com>
2022-08-11 15:11:04 +0000

    main edit

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "aws" {
 #  - Security group 
 
 resource "aws_security_group" "allow_SSH" {
-  name        = "allow_SSH1"
+  name        = "allow_SSH"
   description = "Allow SSH inbound traffic"
 
   #  - INBOUND
@@ -45,7 +45,7 @@ resource "aws_security_group" "allow_SSH" {
 
 resource "aws_key_pair" "deployer1" {
   key_name   = "deployer-key1"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDuHMbZPeFaMWUl8hwwdfjFihMnwu/GawuJdvQ34LJseTT8lKv3HxC5cuCCn/5pIYsjuAjQt/AXGliZjI+qKX0SsTu+ZOQNpi+7ROZGXkeiQGJZfvZteyaG0BylnXvQV7AZiZzE2Nd79mNqFa8Cng3Cztn2qdfjOXxSr5sLDXK0qe8+U7K0umiJ6k/iM3CyKE8IE0dhufDmtk2sDGbZroNircArztwLVK366mj1ECKFcSe/BYdQ2AE/pDqLEcCbJQQDNpkRSTl9sRlG+bjmj4YOZlBkAAu4SLsIcfPTxfHGvqpTwFovHIg0DhxnJpnP4tENOyNbyX33221FGbOgWJu58VhaMM+S4O+Snnw1oqBAToaCv8aCtMFO90NzAP2muLKCsbyLSaQ6ta8ZAix6fwCEmSAeQgbgsjax0fI0m/tYOoflDgaHqqEDlRNDpehVaViV7QrGWfZCmWFjlL9LqLJuzBW1ceRWEbCNmUNXYUz1EPaHNhDrNugB+HitCza72rU= sauravsinghrain@ip-172-31-20-233"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDuaZXADAdi8pqNMOyErPDCOxX1SAWJED6uI2hqIoFZ5jjABcHqHAXdanVjo1xWX+TqoD7fWO3165dRbGOYkHZof2LBRFid+zyjWirYQ/g1O70Lla2qP9YEgQoLLrip2oXWku5fI9VMjrcBgbpCsh+1vaGUKDMYUUNkI5NCEjSuzdxDCxRbVzRsHu1xUswLD1AtrZR+mDBh1NMx7YzbS5XNiVTBFk0UTA10pFf10D61mPm9HnIFLGOFVhMmwfnClLHLWDkZ0jyXNohYlByPPkleTAQbPXzZSkX6GDYCcsCEsf6mmbmZUMHcqz0OvPjbndwpq4+EVWbIebhK/rJDinBaxjhVScP1+L20QZFLUniB/qOeGiHLNWwa+thCDs832WK0A5GXGUz1sGS1XF7nnewQb7hqVFruRwdys8L0KVuY68RsO/qvwyhlOxRVxW2qd95fF3Ld/FzvnwCFP6/ph+64bGm16kVnaDyg1zUCR2lpGveEsxFIJdsMarUp3xHIWYM= sauravsinghrain@ip-172-31-27-154ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDuaZXADAdi8pqNMOyErPDCOxX1SAWJED6uI2hqIoFZ5jjABcHqHAXdanVjo1xWX+TqoD7fWO3165dRbGOYkHZof2LBRFid+zyjWirYQ/g1O70Lla2qP9YEgQoLLrip2oXWku5fI9VMjrcBgbpCsh+1vaGUKDMYUUNkI5NCEjSuzdxDCxRbVzRsHu1xUswLD1AtrZR+mDBh1NMx7YzbS5XNiVTBFk0UTA10pFf10D61mPm9HnIFLGOFVhMmwfnClLHLWDkZ0jyXNohYlByPPkleTAQbPXzZSkX6GDYCcsCEsf6mmbmZUMHcqz0OvPjbndwpq4+EVWbIebhK/rJDinBaxjhVScP1+L20QZFLUniB/qOeGiHLNWwa+thCDs832WK0A5GXGUz1sGS1XF7nnewQb7hqVFruRwdys8L0KVuY68RsO/qvwyhlOxRVxW2qd95fF3Ld/FzvnwCFP6/ph+64bGm16kVnaDyg1zUCR2lpGveEsxFIJdsMarUp3xHIWYM= sauravsinghrain@ip-172-31-27-154"
 }
 
 resource "aws_instance" "amzn-linux" {
